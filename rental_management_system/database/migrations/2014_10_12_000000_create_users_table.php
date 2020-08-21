c52b03f63185bec8c78607df9eb6d36fc7a178a1
winnee0solta <winneecreztha@gmail.com>
2020-08-21 15:36:07 +0545

    laravel project setup complete

--- rental_management_system/database/migrations/2014_10_12_000000_create_users_table.php
+++ rental_management_system/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+             $table->id();
+            $table->string('type');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('email_verified')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
--- rental_management_system/resources/js/app.js
+++ rental_management_system/resources/js/app.js
@@ -1,1 +1,18 @@
-require('./bootstrap');
+require("daemonite-material");
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // var $window = $(window);
+    // var windowsize = $window.width();
+
+    // // //Hide drawer if less width
+    // // if (windowsize < 850) {
+    // //     $("#navdrawerDefault").removeClass("navdrawer-permanent");
+    // //     $("#navdrawerDefault").removeClass("navdrawer-permanent-clipped");
+    // //     $("#drawer_switcher").show();
+    // // }else{
+    // //     // $("#navdrawerDefault").removeClass("navdrawer-permanent");
+    // //      $("#drawer_switcher").hide();
+    // // }
+});
